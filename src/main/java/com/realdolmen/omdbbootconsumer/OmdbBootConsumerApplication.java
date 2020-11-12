a670791d587ac44077dd75a8e154853abd0f8c23
Elias Khan <khan.elias@gmail.com>
2020-11-12 09:26:25 +0100

    Initial commit

---
+++ src/main/java/com/realdolmen/omdbbootconsumer/OmdbBootConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.realdolmen.omdbbootconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdbBootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmdbBootConsumerApplication.class, args);
+    }
+
+}
